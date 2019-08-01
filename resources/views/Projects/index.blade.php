3e1deb10cc2699c85a940c619c9995945587dcd2
Rizwaan <rizwaanbangi@live.co.uk>
2019-08-01 09:44:31 +0100

    button styling implemented

--- resources/views/Projects/index.blade.php
+++ resources/views/Projects/index.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+
 <h1> Projects </h1>
 {{--  <div style="display: flex; align-items: center; padding: 2;">
     <h1 style="margin-right: auto;">"Jim's Gym"</h1>  --}}
 <div class="Flex items-center mb-3">
     <a href="/projects/create">New Project</a>
 </div>
-
+    
 <div class="flex">
     @forelse ($projects as $project)
     <div class="bg-white mr-3 p-2 rounded shadow w-1/3" style="height: 200px">
     
         <h3>
-            <a href="/projects/{{$project->id}}">
+            <a class="project_title"href="/projects/{{$project->id}}">
             {{ $project->title }}
             </a>
         </h3>
@@ -36,4 +37,5 @@
 <a href="/">
 <p>Welcome Page</p>
 </a>
+
 @endsection
