6f439340612b9c4b11b305db97d263180cd5dd53
Rizwaan <rizwaanbangi@live.co.uk>
2019-08-02 13:10:35 +0100

    implemented grid spacing, incorporated a theme, styled buttons and logo

--- resources/views/Projects/index.blade.php
+++ resources/views/Projects/index.blade.php
@@ -2,29 +2,30 @@
 
 @section('content')
 
-<h1> Projects </h1>
 
-<br>
 <header class="Flex items-center mb-3 py-4">
-    <a href="/projects/create" style="color: grey"class="no-underline">Create a New Project</a>
+    <div class="flex justify-between items-center w-full">
+        <h1 class="text-grey text-md font-normal"> My Projects</h1>
+    <a href="/projects/create" style="background-color: lime; box-shadow: 0 2px 7px 0 lime;"class="button_style no-underline py-2 px-5 rounded-lg text-md text-white">Create a New Project</a>
+    </div>
 </header>
-<br>
+
 <div class="introBox">
     <p> Below are the Projects that have been created, click on a project title to find out more details. </p>
 </div>
     
-<div class="flex flex-wrap ">
+<div class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-    <div class="w-1/3 px-3 pb-6 -mx-3">
-        <div class="bg-white p-5 rounded shadow" style="height: 200px">
+    <div class="lg:w-1/3 px-3 pb-6">
+        <div class="bg-white pb-5 pl-5 rounded-lg shadow" style="height: 200px">
         
-            <h3 class="font-normal text-xl py-4">
-                <a class="project_title"href="/projects/{{$project->id}}">
+            <h3 style="background-image: linear-gradient(lime, white);" class="font-normal text-xl py-4 mb-3 -ml-5 pl-4 pr-5">
+                <a class="project_title text-black no-underline"href="/projects/{{$project->id}}">
                 {{ $project->title }}
                 </a>
             </h3>
 
-            <div style ="color: grey">{{ str_limit($project->description), 100 }}</div>
+            <div style ="color: grey"class="">{{ str_limit($project->description), 100 }}</div>
         </div>
     </div>    
     <br>
